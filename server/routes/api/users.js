9d85c4a417151155db97d16c913bde43a6d4ebc2
Nur Islam <nurislam0333@gmail.com>
2018-08-09 17:37:01 +0600

    profile model done

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -25,7 +25,7 @@ router.post('/register', (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
   // Chek Validation
-  if(!isValid) {
+  if (!isValid) {
     return res.status(400).json(errors);
   }
 
@@ -69,7 +69,7 @@ router.post('/login', (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
 
   // Chek Validation
-  if(!isValid) {
+  if (!isValid) {
     return res.status(400).json(errors);
   }
 
