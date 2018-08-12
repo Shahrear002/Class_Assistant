2b84413d95b6707c26ce8bc03d74c49a61df75a3
Shahrear002 <shabuz2900@gmail.com>
2018-08-13 02:17:25 +0600

    Issue Solved

--- server/models/Profile.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const ProfileSchema = new Schema({});
-
-module.exports = Profile = mongoose.model('profile', ProfileSchema);
